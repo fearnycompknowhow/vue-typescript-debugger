7d82c6da09873a7e0db9c96b3698d5c9355acce2
Seth Fearnside <sethfearnside@Fearnys-MacBook-Pro.local>
2021-04-04 15:16:10 -0500

    Store template Source Maps

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const sourceMaps = {};
+
 module.exports = {
   configureWebpack() {
     return {
@@ -6,7 +8,17 @@ module.exports = {
         apply(compiler) {
           compiler.hooks.thisCompilation.tap('Initializing Compilation', (compilation) => {
             compilation.hooks.succeedModule.tap('Module Built', (module) => {
+              const { resource } = module;
 
+              if (!resource) return;
+              if (/node_modules/.test(resource)) return;
+              if (!/\.vue/.test(resource)) return;
+              if (!/type=template/.test(resource)) return;
+              if (!module['_source'] || !module['_source']['_sourceMap']) return;
+
+              const pathWithoutQuery = module.resource.replace(/\?.*$/, '');
+
+              sourceMaps[pathWithoutQuery] = module['_source']['_sourceMap'];
             });
           });
         }
