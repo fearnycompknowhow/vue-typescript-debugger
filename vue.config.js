b86b1dac76eddfe553156d08bda2db01378ccbcd
Seth Fearnside <fearnsides@compknowhow.com>
2022-02-07 16:09:01 -0600

    Added support for Vue CLI v5+

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
+const fs = require('fs');
 const { SourceMapConsumer, SourceMapGenerator } = require('source-map');
-
-const sourceMaps = {};
 
 module.exports = {
   configureWebpack() {
@@ -9,89 +8,17 @@ module.exports = {
       plugins: [{
         apply(compiler) {
           compiler.hooks.thisCompilation.tap('Initializing Compilation', (compilation) => {
-            compilation.hooks.succeedModule.tap('Module Built', (module) => {
-              const { resource } = module;
-
-              if (!resource) return;
-              if (/node_modules/.test(resource)) return;
-              if (!/\.vue/.test(resource)) return;
-              if (!/type=template/.test(resource)) return;
-              if (!module['_source'] || !module['_source']['_sourceMap']) return;
-
-              const pathWithoutQuery = module.resource.replace(/\?.*$/, '');
-
-              sourceMaps[pathWithoutQuery] = module['_source']['_sourceMap'];
-            });
-
-            compilation.hooks.finishModules.tapPromise('All Modules Built', async (modules) => {
+            compilation.hooks.finishModules.tapPromise('Module Built', async (modules) => {
               for (const module of modules) {
-                const { resource } = module;
-
-                if (!resource) continue;
-                if (/node_modules/.test(resource)) continue;
-                if (!/\.vue/.test(resource)) continue;
-                if (!/type=script/.test(resource)) continue;
-                if (!/lang=ts/.test(resource)) continue;
-                if (!module['_source'] || !module['_source']['_sourceMap']) continue;
+                if (shouldSkipModule(module)) continue;
 
                 const pathWithoutQuery = module.resource.replace(/\?.*$/, '');
-                const templateSourceMap = sourceMaps[pathWithoutQuery];
+                const sourceMap = extractSourceMap(module);
+                const sourceFile = fs.readFileSync(pathWithoutQuery).toString('utf-8');
 
-                if (!templateSourceMap) continue;
-
-                const scriptSourceMap = module['_source']['_sourceMap'];
-                scriptSourceMap.sourcesContent = [...templateSourceMap.sourcesContent];
-                scriptSourceMap.sources = [...templateSourceMap.sources];
-
-                const lines = (templateSourceMap.sourcesContent[0] || '').match(/.+/g);
-
-                let indexOfScriptTag = 0;
-
-                for (const line of lines) {
-                  ++indexOfScriptTag;
-                  if (/<script/.test(line)) break;
-                }
-
-                const shiftedSourceMap = await SourceMapConsumer.with(scriptSourceMap, null, async (consumer) => {
-                  const generator = new SourceMapGenerator();
-
-                  consumer.eachMapping((mapping) => {
-                    const {
-                      generatedColumn,
-                      generatedLine,
-                      originalColumn,
-                      originalLine
-                    } = mapping;
-
-                    let name = mapping.name;
-                    let source = templateSourceMap.sources[0] || null;
-
-                    if (originalLine === null || originalColumn === null) {
-                      name = null;
-                      source = null;
-                    }
-                    else {
-                      original = {
-                        column: originalColumn,
-                        line: originalLine + indexOfScriptTag,
-                      };
-                    }
-
-                    generator.addMapping({
-                      generated: {
-                        column: generatedColumn,
-                        line: generatedLine,
-                      },
-                      original,
-                      source,
-                      name
-                    });
-                  });
-
-                  return generator.toJSON();
-                });
-
-                scriptSourceMap.mappings = shiftedSourceMap.mappings;
+                sourceMap.sources = [pathWithoutQuery];
+                sourceMap.sourcesContent = [sourceFile];
+                sourceMap.mappings = await shiftMappings(sourceMap, sourceFile, pathWithoutQuery);
               }
             });
           });
@@ -100,3 +27,85 @@ module.exports = {
     };
   }
 };
+
+function shouldSkipModule(module) {
+  const { resource = '' } = module;
+
+  if (!resource) return true;
+  if (/node_modules/.test(resource)) return true;
+  if (!/\.vue/.test(resource)) return true;
+  if (!/type=script/.test(resource)) return true;
+  if (!/lang=ts/.test(resource)) return true;
+  if (isMissingSourceMap(module)) return true;
+
+  return false;
+}
+
+function isMissingSourceMap(module) {
+  return !extractSourceMap(module);
+}
+
+function extractSourceMap(module) {
+  if (!module['_source']) return null;
+
+  return module['_source']['_sourceMap'] ||
+    module['_source']['_sourceMapAsObject'] ||
+    null;
+}
+
+async function shiftMappings(sourceMap, sourceFile, sourcePath) {
+  const indexOfScriptTag = getIndexOfScriptTag(sourceFile);
+
+  const shiftedSourceMap = await SourceMapConsumer.with(sourceMap, null, async (consumer) => {
+    const generator = new SourceMapGenerator();
+
+    consumer.eachMapping((mapping) => {
+      const {
+        generatedColumn,
+        generatedLine,
+        originalColumn,
+        originalLine
+      } = mapping;
+
+      let name = mapping.name;
+      let source = sourcePath;
+
+      if (originalLine === null || originalColumn === null) {
+        name = null;
+        source = null;
+      }
+      else {
+        original = {
+          column: originalColumn,
+          line: originalLine + indexOfScriptTag,
+        };
+      }
+
+      generator.addMapping({
+        generated: {
+          column: generatedColumn,
+          line: generatedLine,
+        },
+        original,
+        source,
+        name
+      });
+    });
+
+    return generator.toJSON();
+  });
+
+  return shiftedSourceMap.mappings;
+}
+
+function getIndexOfScriptTag(sourceFile) {
+  const lines = sourceFile.match(/.+/g);
+  let indexOfScriptTag = 0;
+
+  for (const line of lines) {
+    ++indexOfScriptTag;
+    if (/<script/.test(line)) break;
+  }
+
+  return indexOfScriptTag;
+}
